47c4c85864ce51585c4fe1edb2b69e1ac3026102
anhbtvn <anhbtvn@gmail.com>
2015-10-12 14:22:59 +0700

    Create css.css.php

---
+++ css.css.php
@@ -0,0 +1,10 @@
+<?php header("Content-type: text/css"); ?>
+@charset utf-8;
+@import url("style.css.php");
+@import url('user.css.php');
+@import url('viewcart.css.php');
+@import url('marquee.css.php');
+@import url('modal.css.php');
+@import url('menupro.css.php');
+@import url('css_menu.css.php');
+@import url('hls-tooltip.css.php');
